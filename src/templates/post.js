80c1f60a9728d449e1e438283c9c486b8d42028a
BrunoBoehm <bruno.boehm@gmail.com>
2019-03-14 20:21:24 +0100

    Make the post template work with graphql and md

--- src/templates/post.js
+++ src/templates/post.js
@@ -1,5 +1,5 @@
-import React from 'react'
-import PropTypes from 'prop-types'
+import React, { Fragment } from 'react'
+// import PropTypes from 'prop-types'
 import { graphql } from 'gatsby'
 
 import { Layout } from '../components/common'
@@ -12,55 +12,78 @@ import { MetaData } from '../components/common/meta'
 *
 */
 const Post = ({ data, location }) => {
-    const post = data.ghostPost
+    const post = data.allMarkdownRemark.edges[0].node
 
     return (
-            <>
-                <MetaData
-                    data={data}
-                    location={location}
-                    type="article"
-                />
-                <Layout>
-                    <div className="container">
-                        <article className="content">
-                            { post.feature_image ?
-                                <figure className="post-feature-image">
-                                    <img src={ post.feature_image } alt={ post.title } />
-                                </figure> : null }
-                            <section className="post-full-content">
-                                <h1 className="content-title">{post.title}</h1>
+        <Fragment>
+            <MetaData
+                data={data}
+                location={location}
+                type="article"
+            />
+            <Layout>
+                <div className="container">
+                    <article className="content">
+                        { post.frontmatter.featured_image ?
+                            <figure className="post-feature-image">
+                                <img src={ post.frontmatter.featured_image.childImageSharp.fixed.src } alt={ post.frontmatter.title } />
+                            </figure> : null }
+                        <section className="post-full-content">
+                            <h1 className="content-title">{post.frontmatter.title}</h1>
 
-                                {/* The main post content */ }
-                                <section
-                                    className="content-body load-external-scripts"
-                                    dangerouslySetInnerHTML={{ __html: post.html }}
-                                />
-                            </section>
-                        </article>
-                    </div>
-                </Layout>
-            </>
+                            {/* The main post content */ }
+                            <section
+                                className="content-body load-external-scripts"
+                                dangerouslySetInnerHTML={{ __html: post.html }}
+                            />
+                        </section>
+                    </article>
+                </div>
+            </Layout>
+        </Fragment>
     )
 }
 
-Post.propTypes = {
-    data: PropTypes.shape({
-        ghostPost: PropTypes.shape({
-            title: PropTypes.string.isRequired,
-            html: PropTypes.string.isRequired,
-            feature_image: PropTypes.string,
-        }).isRequired,
-    }).isRequired,
-    location: PropTypes.object.isRequired,
-}
+// Post.propTypes = {
+//     data: PropTypes.shape({
+//         ghostPost: PropTypes.shape({
+//             title: PropTypes.string.isRequired,
+//             html: PropTypes.string.isRequired,
+//             feature_image: PropTypes.string,
+//         }).isRequired,
+//     }).isRequired,
+//     location: PropTypes.object.isRequired,
+// }
 
 export default Post
 
 export const postQuery = graphql`
-    query($slug: String!) {
-        ghostPost(slug: { eq: $slug }) {
-            ...GhostPostFields
+    query singleMarkdownArticle($slug: String!) {
+        allMarkdownRemark(filter: {frontmatter: {slug: {eq: $slug}}, fileAbsolutePath: {regex: "\/articles/"}}) {
+            edges {
+                node {
+                    frontmatter {
+                        slug
+                        date
+                        title
+                        desc
+                        featured_image {
+                            childImageSharp {
+                              fixed(width: 300) {
+                                src
+                              }
+                            }
+                        }
+                        author {
+                            frontmatter {
+                              author_id
+                              slug
+                            }
+                        }
+                    }
+                    html
+                }
+            }
         }
     }
 `
