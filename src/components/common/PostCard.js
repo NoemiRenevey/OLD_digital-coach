1250fd3126681ed5e47ab027ed9720977a17170f
BrunoBoehm <bruno.boehm@gmail.com>
2019-03-19 13:22:02 +0100

    Improve modularity of postcard with consts

--- src/components/common/PostCard.js
+++ src/components/common/PostCard.js
@@ -9,15 +9,19 @@ const PostCard = ({ post }) => {
     const url = `/${post.frontmatter.slug}/`
     const tools = post.frontmatter.tools
     const goals = post.frontmatter.goals
+    const featuredImg = post.frontmatter.featured_image
+    const category = post.frontmatter.category
+    const title = post.frontmatter.title
+    const excerpt = post.frontmatter.desc
     // const author = post.frontmatter.author ? post.frontmatter.author.frontmatter.author_id : `Lyketil` 
     // const authorSlug = post.frontmatter.author ? post.frontmatter.author.frontmatter.slug : null
 
     return (
         <Link to={url} className="post-card">
             <header className="post-card-header">
-                {post.frontmatter.featured_image &&
+                {featuredImg &&
                     <div className="post-card-image" style={{
-                        backgroundImage: `url(${post.frontmatter.featured_image.childImageSharp.fixed.src})` ,
+                        backgroundImage: `url(${featuredImg.childImageSharp.fixed.src})` ,
                     }}></div>
                 }
                 {goals &&
@@ -28,12 +32,14 @@ const PostCard = ({ post }) => {
                         ])}
                     </div>
                 }
-                {post.frontmatter.category &&
-                    <span><Link to={`/${post.frontmatter.category.slug}`}>{post.frontmatter.category.short_title}</Link></span>
+                {category &&
+                    <span><Link to={`/${category.slug}`}>{category.short_title}</Link></span>
                 }
-                <h2 className="post-card-title">{post.frontmatter.title}</h2>
+                <h2 className="post-card-title">{title}</h2>
             </header>
-            <section className="post-card-excerpt">{post.frontmatter.desc}</section>
+            
+            <section className="post-card-excerpt">{excerpt}</section>
+
             <footer className="post-card-footer">
                 {tools && 
                 <div className="post-card-footer-left">
@@ -43,7 +49,7 @@ const PostCard = ({ post }) => {
                                 <div className="post-card-avatar">
                                     <img className="author-profile-image" src={tool.logo.childImageSharp.fixed.src} alt={tool.name} />
                                 </div>
-                                <span><Link to={`/outils/${tool.id}`}>{tool.name}</Link></span>
+                                <span><Link to={`/outils-digitaux/${tool.id}`}>{tool.name}</Link></span>
                             </Fragment>
                         )
                     })}
--- src/templates/index.js
+++ src/templates/index.js
@@ -91,6 +91,7 @@ export const pageQuery = graphql`
                         }
                         complexity
                         tools {
+                            id
                             name
                             logo {
                                 childImageSharp {
