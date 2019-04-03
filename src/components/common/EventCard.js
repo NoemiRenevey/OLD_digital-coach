cadf9d307758b5b6a07a9a80e1076cf5d74afd9e
BrunoBoehm <bruno.boehm@gmail.com>
2019-04-03 20:51:31 +0200

    Add event card component

--- src/components/common/EventCard.js
+++ src/components/common/EventCard.js
@@ -1,5 +1,7 @@
 import React from 'react'
 
+import { MdConfirmationNumber, MdLiveTv, MdLocationCity } from "react-icons/md";
+
 /** @jsx jsx */
 import { jsx, css } from '@emotion/core'
 import { colors } from '../../styles/constants'
@@ -8,7 +10,7 @@ const EventCard = ({ event }) => {
     const eventSlug = event.slug
     const eventName = event.name
     const excerpt = event.excerpt
-    const date = event.date
+    const eventDate = event.date
     const location = event.location
     const city = event.city
     const type = event.type
@@ -16,11 +18,42 @@ const EventCard = ({ event }) => {
     const eventLink = event.link
     const eventLinkCta = event.link_cta
 
+    const eventType = (type) => {
+        switch (type) {
+        case `event`:
+            return <span><MdLocationCity /> Événement</span>
+        case `webinar`:
+            return <span><MdLiveTv /> Webinar</span>
+        default:
+            return null
+        }
+    } 
+
     return (
         <div css={eventCard}>
+            <div css={eventMeta}>
+                {eventType(type)} le <span>{eventDate}</span>
+            </div>
             <h4>{eventName}</h4>
             <p>{excerpt}</p>
-            <a href={eventLink} alt={eventSlug}>{eventLinkCta}</a>
+
+            {city &&
+                <div css={eventVenue}>
+                    <div>
+                        {location ? `${location} | ` : null}
+                        {city}
+                    </div>
+
+                    {address &&
+                        <div>
+                            {address}
+                        </div>
+                    }
+                </div>
+            }
+
+            {eventLink ? <div css={eventCTA}><a href={eventLink} alt={eventSlug}><MdConfirmationNumber /> {eventLinkCta}</a></div>
+                : eventLinkCta ? <div css={eventCTA}><MdConfirmationNumber /> {eventLinkCta}</div> : null}
         </div>
     )
 }
@@ -35,6 +68,39 @@ const eventCard = css`
     padding: 20px;
 
     h4 {
-        margin-top: 0;
+        margin-top: 10px;
     }
+`
+
+const eventMeta = css`
+    font-size: 1.4rem;
+    color: ${colors.accent};
+
+    svg {
+        margin-bottom: 3px;
+    }
+`
+const eventCTA = css`
+    background-color: ${colors.accent3};
+    display: inline-block;
+    color: white;
+    padding: 5px 14px 7px;
+    border-radius: 5px;
+    text-transform: uppercase;
+
+    a {
+        color: white;
+    }
+
+    svg {
+        margin-bottom: 3px;
+    }
+`
+
+const eventVenue = css`
+    font-size: 1.2rem;
+    line-height: 1.4rem;
+    background-color: ${colors.whitegrey};
+    padding: 10px 20px;
+    margin-bottom: 15px;
 `
