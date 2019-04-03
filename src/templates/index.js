7ae0dfa1632f261ede278ceb3d0ddaac4a553b51
BrunoBoehm <bruno.boehm@gmail.com>
2019-04-03 17:36:08 +0200

    Add basic product pitch to HP

--- src/templates/index.js
+++ src/templates/index.js
@@ -6,6 +6,10 @@ import { Layout, PostCard, Pagination } from '../components/common'
 // import { MetaData } from '../components/common/meta'
 // import { postFields } from '../utils/fragments'
 
+/** @jsx jsx */
+import { jsx, css } from '@emotion/core'
+import { colors } from '../styles/constants'
+
 /**
 * Main index page (home page)
 *
@@ -23,6 +27,25 @@ const Index = ({ data, location, pageContext }) => {
             {/*<MetaData location={location} />*/}
             <Layout isHome={true}>
                 <div className="container">
+                    <section css={productPitch}>
+                        <div css={leftProductPitch}>
+                            <h3>En Bref!</h3>
+                            <h2>Développez votre business grâce à internet... en toute autonomie</h2>
+                            <p>Mettez en place nos "recettes digitales", elles décrivent précisément quoi faire pour atteindre un objectif donné. Il n'y a plus qu'à suivre la méthode : vous (ou votre équipe) devenez indépendant.</p>
+                            <div css={perfectSolution}>
+                                <h4>La solution parfaite pour ceux qui...</h4>
+                                <ul>
+                                    <li>Veulent des solutions concrètes plutôt que de la théorie abstraite</li>
+                                    <li>Souhaitent des résultats rapides, sans engager une agence web</li>
+                                    <li>Savent que le digital est incontournable pour le futur de leur business</li>
+                                </ul>
+                            </div>
+                        </div>
+                        <div css={rightProductPitch}>
+                            <img src="https://place-hold.it/300x300" alt="placeholder"/>
+                        </div>
+                    </section>
+
                     <section className="post-feed">
                         {posts.map(({ node }) => (
                             // The tag below includes the markup for each post - components/common/PostCard.js
@@ -46,6 +69,45 @@ const Index = ({ data, location, pageContext }) => {
 // }
 
 export default Index
+
+/**
+ * 
+ * CSS
+ * 
+ */
+const productPitch = css`
+    display: flex;
+    justify-content: space-between;
+    margin-bottom: 30px;
+`
+
+const leftProductPitch = css`
+    flex-grow: 2;
+    flex-basis: 0;
+
+    h3 {
+        margin-top: 0;
+    }
+
+    h2 {
+        margin-top: .2rem;
+        font-size: 3.6rem;
+    }
+
+    p {
+        font-size: 1.8rem;
+        line-height: 2.6rem;
+    }
+`
+
+const perfectSolution = css`
+
+`
+
+const rightProductPitch = css`
+    flex-basis: 0;
+    flex-grow: 1;
+`
 
 export const pageQuery = graphql`
     query MarkdownArticlesQuery {
