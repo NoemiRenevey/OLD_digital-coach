bfbd636b261cd11deb5292d72434ece953ac7dff
BrunoBoehm <bruno.boehm@gmail.com>
2019-04-04 12:28:38 +0200

    Modularize product pitch

---
+++ src/components/sections/ProductPitch.js
@@ -0,0 +1,60 @@
+import React from 'react'
+
+/** @jsx jsx */
+import { jsx, css } from '@emotion/core'
+
+const ProductPitch = () => {
+    return (
+        <section css={productPitch}>
+            <div css={leftProductPitch}>
+                <h3>En Bref!</h3>
+                <h2>Développez votre business grâce à internet... en toute autonomie</h2>
+                <p className="big">Mettez en place nos "recettes digitales", elles décrivent précisément quoi faire pour atteindre un objectif donné. Il n'y a plus qu'à suivre la méthode : vous (ou votre équipe) devenez indépendant.</p>
+                <div css={perfectSolution}>
+                    <h4>La solution parfaite pour ceux qui...</h4>
+                    <ul>
+                        <li>Veulent des solutions concrètes plutôt que de la théorie abstraite</li>
+                        <li>Souhaitent des résultats rapides, sans engager une agence web</li>
+                        <li>Savent que le digital est incontournable pour le futur de leur business</li>
+                    </ul>
+                </div>
+            </div>
+            <div css={rightProductPitch}>
+                <div css={imagePitch}>
+                    <img src="https://place-hold.it/300x300" alt="placeholder"/>
+                </div>
+            </div>
+        </section>
+    )
+}
+
+export default ProductPitch
+
+/***
+ * CSS
+ */
+
+const productPitch = css`
+    display: flex;
+    justify-content: space-between;
+    margin-bottom: 30px;
+`
+
+const leftProductPitch = css`
+    flex-grow: 2;
+    flex-basis: 0;
+    max-width: 600px;
+`
+
+const perfectSolution = css`
+
+`
+
+const rightProductPitch = css`
+    flex-basis: 0;
+    flex-grow: 1;
+`
+
+const imagePitch = css`
+    text-align: right;
+`
--- src/templates/index.js
+++ src/templates/index.js
@@ -11,6 +11,7 @@ import EventCard from '../components/common/EventCard'
 /** @jsx jsx */
 import { jsx, css } from '@emotion/core'
 import { colors } from '../styles/constants'
+import ProductPitch from '../components/sections/ProductPitch'
 
 /**
 * Main index page (home page)
@@ -30,26 +31,7 @@ const Index = ({ data, location, pageContext }) => {
             {/*<MetaData location={location} />*/}
             <Layout isHome={true}>
                 <div className="container" css={homePage}>
-                    <section css={productPitch}>
-                        <div css={leftProductPitch}>
-                            <h3>En Bref!</h3>
-                            <h2>Développez votre business grâce à internet... en toute autonomie</h2>
-                            <p className="big">Mettez en place nos "recettes digitales", elles décrivent précisément quoi faire pour atteindre un objectif donné. Il n'y a plus qu'à suivre la méthode : vous (ou votre équipe) devenez indépendant.</p>
-                            <div css={perfectSolution}>
-                                <h4>La solution parfaite pour ceux qui...</h4>
-                                <ul>
-                                    <li>Veulent des solutions concrètes plutôt que de la théorie abstraite</li>
-                                    <li>Souhaitent des résultats rapides, sans engager une agence web</li>
-                                    <li>Savent que le digital est incontournable pour le futur de leur business</li>
-                                </ul>
-                            </div>
-                        </div>
-                        <div css={rightProductPitch}>
-                            <div css={imagePitch}>
-                                <img src="https://place-hold.it/300x300" alt="placeholder"/>
-                            </div>
-                        </div>
-                    </section>
+                    <ProductPitch />
 
                     <section css={popularArticles}>
                         <div css={popularIntro}>
@@ -106,31 +88,6 @@ const homePage = css`
     h2 {
         margin-top: .2rem;
     }
-`
-
-const productPitch = css`
-    display: flex;
-    justify-content: space-between;
-    margin-bottom: 30px;
-`
-
-const leftProductPitch = css`
-    flex-grow: 2;
-    flex-basis: 0;
-    max-width: 600px;
-`
-
-const perfectSolution = css`
-
-`
-
-const rightProductPitch = css`
-    flex-basis: 0;
-    flex-grow: 1;
-`
-
-const imagePitch = css`
-    text-align: right;
 `
 
 // Popular Articles
@@ -229,7 +186,7 @@ export const pageQuery = graphql`
                 }
             }
         }
-        events: allEventsYaml {
+        events: allEventsYaml(limit: 3) {
             edges {
               node {
                 slug
