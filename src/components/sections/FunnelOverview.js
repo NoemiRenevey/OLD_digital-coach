7d2b35c10738ea2c444f1a2a86be3fe8c44b766d
BrunoBoehm <bruno.boehm@gmail.com>
2019-04-05 17:04:05 +0200

    Start styling the funnel chats

--- src/components/sections/FunnelOverview.js
+++ src/components/sections/FunnelOverview.js
@@ -27,12 +27,13 @@ const FunnelOverviewComp = ({ data }) => {
             <div css={funnelViz}>
                 {objectives.map(({ node: objective }, i) => (
                     <div key={objective.id}>
-                        <div css={funnelStage} style={{ width: `${100-i*5}%` }}>
-                            <h4>{objective.name}</h4>
+                        <div css={funnelStage} style={{ width: `${100-i*5}%` }} className="slideInUp">
+                            <h4><span className="stabilo">Objectif :</span> {objective.name}</h4>
+
                             {(objective.buyer_exp || objective.seller_exp) && 
                                 <ul>
-                                    {objective.buyer_exp && <li>{objective.buyer_exp}</li>}
                                     {objective.seller_exp && <li>{objective.seller_exp}</li>}
+                                    {objective.buyer_exp && <li>{objective.buyer_exp}</li>}
                                 </ul>
                             }
                         </div>
@@ -93,12 +94,37 @@ const funnelViz = css`
 `
 
 const funnelStage = css`
-    background-color: ${colors.whitegrey};
+    background-image: url("data:image/svg+xml,%3Csvg width='52' height='26' viewBox='0 0 52 26' xmlns='http://www.w3.org/2000/svg'%3E%3Cg fill='none' fill-rule='evenodd'%3E%3Cg fill='%23f0f0f0' fill-opacity='0.4'%3E%3Cpath d='M10 10c0-2.21-1.79-4-4-4-3.314 0-6-2.686-6-6h2c0 2.21 1.79 4 4 4 3.314 0 6 2.686 6 6 0 2.21 1.79 4 4 4 3.314 0 6 2.686 6 6 0 2.21 1.79 4 4 4v2c-3.314 0-6-2.686-6-6 0-2.21-1.79-4-4-4-3.314 0-6-2.686-6-6zm25.464-1.95l8.486 8.486-1.414 1.414-8.486-8.486 1.414-1.414z' /%3E%3C/g%3E%3C/g%3E%3C/svg%3E");
     border-radius: 5px;
+    border: 1px solid ${colors.whitegrey};
     margin-bottom: 20px;
     padding: 20px 20px;
 
     h4 {
         margin-top: 0;
+    }
+
+    ul {
+        margin-bottom: 0;
+        list-style: none;
+        padding-left: 0;
+
+        li {
+            padding: 4px 20px;
+            border-radius: 20px;
+            display: inline-block;
+        }
+
+        li:first-child {
+            background-color: ${colors.accent3};
+            color: white;
+            border-bottom-left-radius: 5px;
+        }
+
+        li:nth-child(2) {
+            background-color: ${colors.whitegrey};
+            border-bottom-right-radius: 5px;
+            text-align: right;
+        }
     }
 `
