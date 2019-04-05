bd681248a9294d0f79e0a0824d9288262855b834
BrunoBoehm <bruno.boehm@gmail.com>
2019-04-05 16:52:41 +0200

    Basic styling of funnel

--- src/components/sections/FunnelOverview.js
+++ src/components/sections/FunnelOverview.js
@@ -7,26 +7,27 @@ import { colors } from '../../styles/constants'
 
 const FunnelOverviewComp = ({ data }) => {
     const objectives = data.allGoalsYaml.edges
-    const borderColor = ( colorCode ) => {
+    const borderColor = (colorCode) => {
         if (colorCode) {
             // console.log("Color:", colors[colorCode])
-            return { borderImage: `4px solid ${colors[colorCode]}` }
+            // Put in div style={borderColor(objective.color_code)}
+            return { 
+                borderStyle: `solid`,
+                borderWidth: `4px`,
+                borderImage: `${colors[colorCode]} 1 100%`,
+            }
         } else {
-            return { borderLeft: `4px solid #fff` }
+            return { borderLeft: `4px solid #F4F4F4` }
         }
     }
 
     return (
         <section css={funnelSection}>
-            <div css={funnelDesc}>
-                <h2>Des Playbooks pour chacun de vos défis</h2>
-                <p className="big">Pour tous les défis digitaux que rencontre votre entreprise, nous avons rassemblé et continuons chaque jour d'apporter des solutions : de l'acquisition de clients jusqu'à l'automatisation des process dans votre PME, il existe un Playbook adapté !</p>
-            </div>
  
             <div css={funnelViz}>
-                {objectives.map(({ node: objective }) => (
+                {objectives.map(({ node: objective }, i) => (
                     <div key={objective.id}>
-                        <div style={borderColor(objective.color_code)}>
+                        <div css={funnelStage} style={{ width: `${100-i*5}%` }}>
                             <h4>{objective.name}</h4>
                             {(objective.buyer_exp || objective.seller_exp) && 
                                 <ul>
@@ -38,6 +39,12 @@ const FunnelOverviewComp = ({ data }) => {
                     </div>
                 ))}
             </div>
+
+            <div css={funnelDesc}>
+                <h2>Des Playbooks pour chacun de vos défis</h2>
+                <p className="big">Pour tous les défis digitaux que rencontre votre entreprise, nous avons rassemblé et continuons chaque jour d'apporter des solutions : de l'acquisition de clients jusqu'à l'automatisation des process dans votre PME, il existe un Playbook adapté !</p>
+            </div>
+
         </section>
     )
 }
@@ -72,12 +79,26 @@ export default FunnelOverview
 const funnelSection = css`
     margin-top: 80px;
     margin-bottom: 80px;
+    display: flex;
+    flex-direction: row;
+    justify-content: space-between;
 `
 
 const funnelDesc = css`
-
+    width: 40%;
 `
 
 const funnelViz = css`
+    width: 55%;
+`
 
+const funnelStage = css`
+    background-color: ${colors.whitegrey};
+    border-radius: 5px;
+    margin-bottom: 20px;
+    padding: 20px 20px;
+
+    h4 {
+        margin-top: 0;
+    }
 `
