c4948a91c8013a917ce783bb5a3611ced79bc6f5
BrunoBoehm <bruno.boehm@gmail.com>
2019-04-07 12:30:06 +0200

    Improve goals descriptions

--- src/components/sections/FunnelOverview.js
+++ src/components/sections/FunnelOverview.js
@@ -50,7 +50,7 @@ class FunnelOverviewComp extends React.Component {
                 <div css={funnelViz}>
                     {objectives.map(({ node: objective }, i) => {
                         const borderStyle = borderColor(objective.color_code)
-                        const widthStyle = { width: `${100 - i * 5}%` }
+                        const widthStyle = { width: `${100 - i * 2}%` }
     
                         return (
                             <div key={objective.id}>
@@ -136,7 +136,7 @@ const funnelStage = css`
     background-image: url("data:image/svg+xml,%3Csvg width='52' height='26' viewBox='0 0 52 26' xmlns='http://www.w3.org/2000/svg'%3E%3Cg fill='none' fill-rule='evenodd'%3E%3Cg fill='%23f0f0f0' fill-opacity='0.4'%3E%3Cpath d='M10 10c0-2.21-1.79-4-4-4-3.314 0-6-2.686-6-6h2c0 2.21 1.79 4 4 4 3.314 0 6 2.686 6 6 0 2.21 1.79 4 4 4 3.314 0 6 2.686 6 6 0 2.21 1.79 4 4 4v2c-3.314 0-6-2.686-6-6 0-2.21-1.79-4-4-4-3.314 0-6-2.686-6-6zm25.464-1.95l8.486 8.486-1.414 1.414-8.486-8.486 1.414-1.414z' /%3E%3C/g%3E%3C/g%3E%3C/svg%3E");
     border-radius: 5px;
     border: 1px solid ${colors.whitegrey};
-    margin-bottom: 20px;
+    margin-bottom: 5px;
     padding: 20px 20px 10px;
 
     h4 {
