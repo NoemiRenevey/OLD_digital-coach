84cd0f84420ca9e69547c94dfdaa13aef4e2f9f5
BrunoBoehm <bruno.boehm@gmail.com>
2019-04-07 19:13:32 +0200

    Add tool metadata

--- src/templates/tool.js
+++ src/templates/tool.js
@@ -2,6 +2,8 @@ import React, { Fragment } from 'react'
 // import PropTypes from 'prop-types'
 import { graphql } from 'gatsby'
 
+import { MdOpenInNew, MdLanguage, MdFlag, MdMonetizationOn } from "react-icons/md"
+
 import { Layout, PostCard, Pagination } from '../components/common'
 import { MetaData } from '../components/common/meta'
 
@@ -32,6 +34,15 @@ const Tool = ({ data, location, pageContext }) => {
                         <div css={toolDesc}>                        
                             <h1>{tool.name}</h1>
                             <p className="content-intro">{tool.desc}</p>
+
+                            <div css={toolMeta}>
+                                <ul>
+                                    {tool.url && <li><MdOpenInNew /> <a href={tool.url} title={tool.name}>{tool.url.replace(/^https?:\/\//,'')}</a></li>}
+                                    {tool.price && <li><MdMonetizationOn /> {tool.price}</li>}
+                                    {tool.country && <li><MdFlag /> {tool.country}</li>}
+                                    {tool.languages && <li><MdLanguage /> {tool.languages.join(`, `)}</li>}
+                                </ul>
+                            </div>
                         </div>
 
                         <div css={toolImg}>
@@ -87,15 +98,43 @@ const toolHeader = css`
 `
 
 const toolDesc = css`
-    width: 50%;
+    width: 80%;
+`
+
+const toolMeta = css`
+    background-image: url("data:image/svg+xml,%3Csvg width='52' height='26' viewBox='0 0 52 26' xmlns='http://www.w3.org/2000/svg'%3E%3Cg fill='none' fill-rule='evenodd'%3E%3Cg fill='%23f0f0f0' fill-opacity='0.4'%3E%3Cpath d='M10 10c0-2.21-1.79-4-4-4-3.314 0-6-2.686-6-6h2c0 2.21 1.79 4 4 4 3.314 0 6 2.686 6 6 0 2.21 1.79 4 4 4 3.314 0 6 2.686 6 6 0 2.21 1.79 4 4 4v2c-3.314 0-6-2.686-6-6 0-2.21-1.79-4-4-4-3.314 0-6-2.686-6-6zm25.464-1.95l8.486 8.486-1.414 1.414-8.486-8.486 1.414-1.414z' /%3E%3C/g%3E%3C/g%3E%3C/svg%3E");
+    border-radius: 5px;
+    border: 1px solid ${colors.whitegrey};
+    margin-bottom: 30px;
+    padding: 20px 20px 10px;
+
+    ul {
+        list-style: none;
+        padding-left: 0;
+        display: flex;
+        justify-content: space-between;
+
+        li {
+            width: 24%;
+            text-align: center;
+
+            svg {
+                padding-bottom: 3px;
+            }
+        }
+    }
 `
 
 const toolImg = css`
     width: 30%;
     text-align: right;
-    height: 120px;
-    width: 120px;
-    border-radius: 100%;
+
+    img {
+        object-fit: cover;
+        height: 120px;
+        width: 120px;
+        border-radius: 100%;
+    }
 `
 
 export const pageQuery = graphql`
@@ -113,6 +152,10 @@ query toolQuery($id: String) {
                     }
                   }
             }
+            price
+            country
+            url
+            languages
           }
         }
     }
